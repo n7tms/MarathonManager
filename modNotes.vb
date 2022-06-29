4e1a2b0c5b3933d601b9a705a161d6175310550d
n7tms <n7tms@yahoo.com>
2022-06-29 15:26:51 -0600

    basic db done

--- modNotes.vb
+++ modNotes.vb
@@ -5,5 +5,29 @@
 
 
 
+    'There may be some value in this code
+    'Dim conn As New SQLiteConnection("Data Source=myDatabase.sql3;Version=3;")
+    '    conn.Open()
+    '    Dim SQL As String = "SELECT * from lang;"
+    'Dim da As New SQLiteDataAdapter(SQL, conn)
+    'Dim ds As New DataSet
+    '    da.Fill(ds, "lang")
+    '    'Close the connection
+    '    conn.Close()
+    '    'ListView1
+    '    With ListView1
+    '.View = View.Details
+    '.HeaderStyle = ColumnHeaderStyle.Nonclickable ' set to whatever you need
+    '.Columns.Clear() ' make sure columnscollection is empty
+    '' Add 3 columns
+    '.Columns.Add(ds.Tables(0).Columns(0).ColumnName)
+    '.Columns.Add(ds.Tables(0).Columns(1).ColumnName)
+    '.Columns.Add(ds.Tables(0).Columns(2).ColumnName)
+
+    'End With
+    'For i As Integer = 0 To ds.Tables(0).Rows.Count - 1
+    '        ListView1.Items.Add(New ListViewItem({ds.Tables(0).Rows(i).Item(0).ToString(), ds.Tables(0).Rows(i).Item(1).ToString(), ds.Tables(0).Rows(i).Item(2).ToString()}))
+    '    Next
+
 
 End Module
